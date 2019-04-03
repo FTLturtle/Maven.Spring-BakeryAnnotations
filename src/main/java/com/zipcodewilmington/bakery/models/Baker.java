aaf1b44b64c9596d347cfb07162c848f06125ea1
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-04-03 16:35:54 -0400

    fixed two tests and added annotations

--- src/main/java/com/zipcodewilmington/bakery/models/Baker.java
+++ src/main/java/com/zipcodewilmington/bakery/models/Baker.java
@@ -1,11 +1,21 @@
 package com.zipcodewilmington.bakery.models;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
+@Entity
 public class Baker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private String name;
 
     private String employeeId;
